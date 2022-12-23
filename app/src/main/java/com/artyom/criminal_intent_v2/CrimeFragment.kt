e76ac42bfa6314b26d0d0025ca6a7c0723244809
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2022-12-23 22:16:31 +0700

    Реализовал смену даты

--- app/src/main/java/com/artyom/criminal_intent_v2/CrimeFragment.kt
+++ app/src/main/java/com/artyom/criminal_intent_v2/CrimeFragment.kt
@@ -20,8 +20,9 @@ import java.util.*
 private const val TAG = "CrimeFragment"
 private const val ARG_CRIME_ID = "crime_id"
 private const val DIALOG_DATE = "DialogDate"
+private const val REQUEST_DATE = 0
 
-class CrimeFragment: Fragment() {
+class CrimeFragment: Fragment(), DatePickerFragment.Callbacks {
 
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
@@ -100,7 +101,11 @@ class CrimeFragment: Fragment() {
         }
 
         dateButton.setOnClickListener {
-            DatePickerFragment().apply {
+           /* DatePickerFragment().apply {
+                show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
+            }*/
+            DatePickerFragment.newInstance(crime.date).apply {
+                setTargetFragment(this@CrimeFragment, REQUEST_DATE)
                 show(this@CrimeFragment.requireFragmentManager(), DIALOG_DATE)
             }
         }
@@ -110,6 +115,11 @@ class CrimeFragment: Fragment() {
     override fun onStop() {
         super.onStop()
         crimeDetailViewModel.saveCrime(crime)
+    }
+
+    override fun onDateSelected(date: Date) {
+        crime.date = date
+        updateUI()
     }
 
     private fun updateUI() {
